556b660673ffb4361aede34252a1d779238a9cc9
Annariia <167634401+annikon3@users.noreply.github.com>
2025-03-23 20:20:01 +0200

    testing out colors

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,16 +6,10 @@ from sklearn.svm import LinearSVC
 
 # UI
 st.title("Sentiment Analysis WebApp")
-st.write("Please first train the model. Then, enter a sentence to analyze its sentiment.")
+st.write("Enter feedback to analyze its sentiment.")
 
 # Load and process training data
 def train_model():
-    st.session_state.training = True
-    st.session_state.feedback = "Please wait, training in progress..."
-    # Disable butto nwhile trainign in progrsss
-    st.session_state.disable_train_button = True
-    st.rerun()
-    
     df_train = pd.read_csv("train.csv", encoding='latin-1')
     df_train.dropna(inplace=True)
     X_train = df_train['text']
@@ -27,25 +21,23 @@ def train_model():
         ('clf', LinearSVC())
     ])
     text_clf.fit(X_train, y_train)
-    
-    st.session_state.training = False
-    st.session_state.feedback = "Model trained successfully!"
-    # Enable button
-    st.session_state.disable_train_button = False
-    st.rerun()
+    st.success("Model trained successfully!")
 
-if "training" not in st.session_state:
-    st.session_state.training = False
-    st.session_state.feedback = ""
-    st.session_state.disable_train_button = False
-
-if st.button("Train Model", disabled=st.session_state.disable_train_button):
+if st.button("Train Model"):
     train_model()
 
-st.write(st.session_state.feedback)
+text_input = st.text_input("Please enter your feedback:")
 
-# Text input for entering feedback
-text_input = st.text_input("Enter your feedback:")
-if st.button("Analyze Feedback Sentiment") and text_input:
-    analysis = text_clf.predict([text_input])[0]
-    st.write("Analyzed Sentiment:", analysis)
+if st.button("Analyze Sentiment") and text_input:
+    prediction = text_clf.predict([text_input])[0]
+    
+    color = "black"
+    if prediction == "negative":
+        color = "red"
+    elif prediction == "neutral":
+        color = "yellow"
+    elif prediction == "positive":
+        color = "green"
+    
+    st.markdown(f"<p style='color:{color}; font-size:20px;'>Predicted Sentiment: {prediction}</p>", unsafe_allow_html=True)
+    
